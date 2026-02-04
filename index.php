815da01993fb9afc6ffee611ceb560443f41d5a5
alexandrovsk <alexanderlim762@gmail.com>
2026-02-04 18:50:03 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+require 'check_auth.php';
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+<meta charset="UTF-8">
+<title>Форум</title>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&display=swap" rel="stylesheet">
+<style>
+body {font-family: Montserrat, sans-serif; background:#020617; color:white; padding:20px;}
+</style>
+</head>
+<body>
+<h1>Форум Alexandrovsk</h1>
+<p>Вы успешно авторизованы 🎉</p>
+</body>
+</html>
