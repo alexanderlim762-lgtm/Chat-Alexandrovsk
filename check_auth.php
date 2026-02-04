c164c2bb904b738134d376fd9ee45a109de4f498
alexandrovsk <alexanderlim762@gmail.com>
2026-02-04 20:45:09 +0300

    Update check_auth.php

--- check_auth.php
+++ check_auth.php
@@ -1,6 +1,5 @@
 <?php
 session_start();
-
 if (isset($_SESSION['user_id'])) return;
 
 if (isset($_COOKIE['auth_token'])) {
@@ -8,7 +7,6 @@ if (isset($_COOKIE['auth_token'])) {
     $api = "https://alexandrovsk.c6t.ru/api/check_token.php?token=" . urlencode($token);
     $response = @file_get_contents($api);
     $data = json_decode($response, true);
-
     if ($data && $data['status'] === 'ok') {
         $_SESSION['user_id'] = $data['user_id'];
         $_SESSION['username'] = $data['username'];
@@ -16,7 +14,6 @@ if (isset($_COOKIE['auth_token'])) {
     }
 }
 
-// Редирект на логин
 $redirect = urlencode("https://forum-alexandrovsk.c6t.ru".$_SERVER['REQUEST_URI']);
 header("Location: https://alexandrovsk.c6t.ru/login.html?redirect=$redirect");
 exit;
